51d9a51c9501d4c333af2d6a36f90f3e5eb89938
zoenggamming <zoenggamming@qq.com>
2020-07-30 20:19:37 +0800

    上传VERSION_0.1代码

---
+++ btnCallback.h
@@ -0,0 +1,14 @@
+#ifndef __BTNCALLBACK_H
+#define __BTNCALLBACK_H
+
+#include "easyButton.h"
+#include "stdio.h"
+
+int btnTest_Impending_Callback(void);
+int btnTest_PressDown_Callback(void);
+int btnTest_PressUp_Callback(void);
+int btnTest_SingleClick_Callback(void);
+int btnTest_DoubleClick_Callback(void);
+int btnTest_LongTimeHoldStart_Callback(void);
+int btnTest_LongTimeHold_Callback(void);
+#endif  //__BTNCALLBACK_H
